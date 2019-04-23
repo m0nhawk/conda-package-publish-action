ded75670b3f0b0e99633284e65b48a0a84aa23c6
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2019-04-23 00:19:08 -0500

    chore(formatting): convert tabs => spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,23 @@ set -ex
 set -o pipefail
 
 go_to_build_dir() {
-	if [ ! -z $SUBDIR ]; then
-		cd $SUBDIR
-	fi
+    if [ ! -z $SUBDIR ]; then
+        cd $SUBDIR
+    fi
 }
 
 check_if_setup_file_exists() {
-	if [ ! -f setup.py ]; then
-		echo "setup.py must exist in the directory that is being packaged and published."
-		exit 1
-	fi
+    if [ ! -f setup.py ]; then
+        echo "setup.py must exist in the directory that is being packaged and published."
+        exit 1
+    fi
 }
 
 check_if_meta_yaml_file_exists() {
     if [ ! -f meta.yaml ]; then
-		echo "meta.yaml must exist in the directory that is being packaged and published."
-		exit 1
-	fi
+        echo "meta.yaml must exist in the directory that is being packaged and published."
+        exit 1
+    fi
 }
 
 upload_package(){
